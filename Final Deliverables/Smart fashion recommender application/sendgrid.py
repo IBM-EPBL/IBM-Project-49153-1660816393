10ddb12708b994fca998d05bcd25cbcecaac12c7
Babisdania <114726113+Babisdania@users.noreply.github.com>
2022-11-13 16:22:33 +0530

    Add files via upload

---
+++ Final Deliverables/Smart fashion recommender application/sendgrid.py
@@ -0,0 +1,19 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='ajishaj@gmail.com',
+    to_emails='amirenjai@gmail.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('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'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
